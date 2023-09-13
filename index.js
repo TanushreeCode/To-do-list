9230779e4a25d2a61cf38dc3e35eba8b0062d092
Tanushree <nayaktanu12january@gmail.com>
2023-09-13 11:43:16 +0530

    added js code and made it funtionable

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(){
     const taskInput  = document.getElementById("taskInput");
-    
+     const addTaskButton = document.getElementById("addTask")
+     const taskList = document.getElementById("taskList");
+
+     addTaskButton.addEventListener("click",function(){
+        const taskText = taskInput.value.trim()
+        if(taskText==="")return
+        const li = document.createElement("li")
+        li.innerHTML = `${taskText} <button class="delete-btn">Delete</button>`
+        taskList.appendChild(li)
+        taskInput.value=""
+         
+        const deleteButton = li.querySelector(".delete-btn")
+        deleteButton.addEventListener("click",function(){
+            taskList.removeChild(li)
+        })
+
+     })
+     taskInput.addEventListener("keydown",function(event){
+        if(event.key==="Enter"){
+            addTaskButton.click()
+        }
+     })
 })
